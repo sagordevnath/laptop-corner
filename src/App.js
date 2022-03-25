9f8f69f9e6c50fc8690c80246bc939460fa23883
sagordevnath <ssagor64@gmail.com>
2022-03-25 23:52:38 +0600

    laptop container add

--- src/App.js
+++ src/App.js
@@ -3,25 +3,35 @@ import Laptop from './components/Laptop/Laptop';
 
 function App() {
   const laptops = [
-    {id: 1, name: 'Apple-Macbook', price: 100000},
-    {id: 2, name: 'Lenovo-Yoga', price: 85000},
-    {id: 3, name: 'HP-Ellitebook', price: 62000},
-    {id: 4, name: 'Dell-inspiron', price: 55000},
-    {id: 5, name: 'Asus-Expertbook', price: 43000},
-    {id: 6, name: 'Acer-Ferrari', price: 52000},
-    {id: 7, name: 'Walton-Passion', price: 30000},
-    {id: 8, name: 'Fujitsu-Lifebook', price: 49000},
-    {id: 9, name: 'Samsung-Sens', price: 70000},
+    {id: 1, image: 'http://surl.li/bprbh', name: 'Apple-Macbook', price: 100000},
+    {id: 2, image: 'http://surl.li/bprbh', name: 'Lenovo-Yoga', price: 85000},
+    {id: 3, image: 'http://surl.li/bprbh', name: 'HP-Ellitebook', price: 62000},
+    {id: 4, image: 'http://surl.li/bprbh', name: 'Dell-inspiron', price: 55000},
+    {id: 5, image: 'http://surl.li/bprbh', name: 'Asus-Expertbook', price: 43000},
+    {id: 6, image: 'http://surl.li/bprbh', name: 'Acer-Ferrari', price: 52000},
+    {id: 7, image: 'http://surl.li/bprbh', name: 'Walton-Passion', price: 30000},
+    {id: 8, image: 'http://surl.li/bprbh', name: 'Fujitsu-Lifebook', price: 49000},
+    {id: 9, image: 'http://surl.li/bprbh', name: 'Samsung-Sens', price: 70000},
   ];
 
   
 
   return ( 
-    <div className = "App" >
-    <h1> Laptop Corner </h1> 
-    {
-      laptops.map(laptop => <Laptop key={laptop.id} laptop={laptop}></Laptop>)
-    }
+    <div className='App'>
+      <h1> Laptop Corner </h1> 
+      <h3>Choose your favorite Laptop</h3>
+      <div className = "main-container" >
+        <div className='laptop-container'>
+        {
+          laptops.map(laptop => <Laptop key={laptop.id} laptop={laptop}></Laptop>)
+        }
+        </div>
+        <div className='cart-container'>
+          <div>
+            <h3>Selected Laptop</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Laptop/Laptop.js
+++ src/components/Laptop/Laptop.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Laptop.css';
 
-const Laptop = (props) => {
-    console.log(props)
+const Laptop = ({laptop}) => {
+    const {image, name, price} = laptop;
     return (
-        <div>
-            
+        <div className='laptop-info'>
+            <img src={image} alt=""/>
+            <h4>Name: {name}</h4>
+            <p>Price: {price}</p>
+            <button className='btn'>Add to Cart </button>
         </div>
     );
 };
